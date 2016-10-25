45abf9e850315ed5ee6bd4ae83f5fa8150ba48d9
Johan Cwiklinski <johan@x-tnd.be>
2016-10-25 21:21:37 +0200

    Add transifex config

--- source/conf.py
+++ source/conf.py
@@ -270,3 +270,7 @@ texinfo_documents = [
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
+
+#intl stuff
+locale_dirs = ['locale/']   # path is example but recommended.
+gettext_compact = False     # optional.
